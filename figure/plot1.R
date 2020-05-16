21a727c8a02abc8ba1d9aeefb3e43e38ecdb9b5b
michaelannold <47949399+michaelannold@users.noreply.github.com>
2020-05-16 01:07:08 +0000

    Add files via upload

---
+++ figure/plot1.R
@@ -0,0 +1,7 @@
+datafile <- "./data/household_power_consumption.txt"
+data <- read.table(datafile, header = TRUE, sep = ";", stringsAsFactors = FALSE, dec = ".")
+subsetdata <- data[data$Date %in% c("1/2/2007", "2/2/2007"), ]
+globalactive <- as.numeric(subsetdata$Global_active_power)
+png("plot1.png", width = 480, height = 480)
+hist(globalactive, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+dev.off()
